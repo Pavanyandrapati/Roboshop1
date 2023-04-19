37241cd556ab5420b838d5602c45f0bc71b2fed5
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-04-19 21:45:16 +0530

    frontend

---
+++ catalogue.sh
@@ -0,0 +1,17 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+rm -rf /app
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+npm install
+
+cp /root/Roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+cp /root/Roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --host mongodb-dev.pavan345.online </app/schema/catalogue.js
