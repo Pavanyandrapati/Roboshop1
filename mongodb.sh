11b7f6aa12fb30c479f183975c2ec4b7f53057de
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-04-19 21:57:35 +0530

    frontend

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,6 @@
 yum install mongodb-org -y
 cp /root/Roboshop1/mongo.repo /etc/yum.repos.d/mongo.repo
-
 systemctl enable mongod
 systemctl start mongod
-
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl restart mongod
