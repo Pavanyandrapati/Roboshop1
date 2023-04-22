213917f8fa7abbd06db645b5bb24675d9b76e330
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-04-22 22:21:43 +0530

    frontend

--- mysql.sh
+++ mysql.sh
@@ -2,10 +2,13 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
+
 if [ -z "$mysql_root_password"];then
   echo input missing
   exit
 fi
+
+
 func_print_head "disable mysql"
 dnf module disable mysql -y &>>$log_file
 func_stat_check $?
