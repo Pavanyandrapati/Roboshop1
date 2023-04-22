9c8f81d1d7e06fee7970d40ba2131211d58e0f48
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-04-22 17:09:14 +0530

    frontend

--- mongodb.sh
+++ mongodb.sh
@@ -4,19 +4,19 @@ source $script_path/common.sh
 
 func_print_head "Copying mongo repo"
 cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
-func_stat_check $1
+func_stat_check $?
 
 echo -e "\e[31m>>>> install mongodb <<<<\e[0m"
 func_print_head "install mongodb"
 yum install mongodb-org -y &>>$log_file
-func_stat_check $1
+func_stat_check $?
 
 func_print_head "start mongodb services"
 systemctl enable mongod &>>$log_file
 systemctl start mongod &>>$log_file
-func_stat_check $1
+func_stat_check $?
 
 func_print_head "change hosts"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$log_file
-func_stat_check $1
+func_stat_check $?
 systemctl restart mongod &>>$log_file
