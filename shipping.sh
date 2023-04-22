61914d26c2492a00cb5eb8384a07a51ca1fe5d8c
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-04-22 13:25:15 +0530

    frontend

--- shipping.sh
+++ shipping.sh
@@ -3,12 +3,13 @@ script_path=$(dirname "$script")
 source $script_path/common.sh
 mysql_root_password=$1
 
-if [ -z "$mysql_root_password"]; then
+ if [ -z "$mysql_root_password"]; then
    echo input missing
    exit
-fi
-component='shipping"
-schema_setup=mysql
-func_java
+ fi
 
+ component="shipping"
 
+ schema_setup=mysql
+
+ func_java
