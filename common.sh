c98c49f392fe30971d3d75568305353ee62d816b
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-04-22 13:15:55 +0530

    frontend

--- common.sh
+++ common.sh
@@ -22,9 +22,40 @@ func_app_prereq() {
   unzip /tmp/${component}.zip
   cd /app
 
-  }
+ }
+ func_schema_setup() {
+      if [ "$schema_setup" == "mongo" ]; then
+     func_print_head "Copying mongo repo"
+     cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
 
-  func_nodejs() {
+     func_print_head "installing mongodb"
+     yum install mongodb-org-shell -y
+
+     func_print_head "load schema"
+      mongo --host mongodb-dev.pavan345.online </app/schema/catalogue.js
+     fi
+     if [ "${schema_setup}" == "mysql" ]; then
+
+     func_print_head "install mysql"
+     yum install mysql -y
+
+     func_print_head "load schema"
+      mysql -h mysql-dev.pavan345.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+     fi
+ }
+func_systemd_setup() {
+
+      func_print_head"copying ${component service}"
+      cp $script_path/${component}.service /etc/systemd/system/${component}.service
+
+      func_print_head "${component service}"
+
+      systemctl daemon-reload
+      systemctl enable ${component}
+      systemctl start ${component}
+
+}
+func_nodejs() {
 
     func_print_head "Downloading content"
     curl -sL https://rpm.nodesource.com/setup_lts.x | bash
@@ -41,10 +72,10 @@ func_app_prereq() {
 
     func_schema_setup
 
-    }
+}
 
-      func_java() {
-      func_print_head "install maven"
+func_java() {
+     func_print_head "install maven"
       yum install maven -y
 
       func_print_head "download maven dependencies"
@@ -55,35 +86,5 @@ func_app_prereq() {
 
       func_systemd_setup
       func_schema_setup
-      }
-func_schema_setup() {
-  if [ "$schema_setup" == "mongo" ]; then
-    func_print_head "Copying mongo repo"
-    cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
-
-    func_print_head "installing mongodb"
-    yum install mongodb-org-shell -y
-
-    func_print_head "load schema"
-     mongo --host mongodb-dev.pavan345.online </app/schema/catalogue.js
-  fi
-  if [ "${schema_setup}" == "mysql" ]; then
-
-    func_print_head "install mysql"
-    yum install mysql -y
-
-    func_print_head "load schema"
-    mysql -h mysql-dev.pavan345.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-fi
 }
-  func_systemd_setup() {
 
-    func_print_head"copying ${component service}"
-      cp $script_path/${component}.service /etc/systemd/system/${component}.service
-
-      func_print_head "${component service}"
-
-      systemctl daemon-reload
-      systemctl enable ${component}
-      systemctl start ${component}
-  }
