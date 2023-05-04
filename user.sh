63691d8b7f6fef928e790eba65583c1a81ed96a8
Pavan yandrapati <pavanyandrapati345@gmail.com>
2023-05-05 00:06:28 +0530

    frontend

--- user.sh
+++ user.sh
@@ -1,8 +1,11 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+func_nodejs
 
-component=user
-schema_setup=mongo
+component = user
+
+schema_setup = mongo
+
 func_nodejs
 
